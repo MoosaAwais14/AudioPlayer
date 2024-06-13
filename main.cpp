fb4e92fd6379b80c1696ccc9e837074de89be92f
MoosaAwais14 <122404861+MoosaAwais14@users.noreply.github.com>
2024-06-13 03:05:52 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,35 +2,32 @@
 #include <string>       
 #include <Windows.h> // Include the Windows library for Windows-specific functions
 #include <mmsystem.h> // Include the mmsystem library for multimedia functions
-using namespace std;     
+using namespace std;
 
 #pragma comment(lib, "winmm.lib")  // Link the winmm.lib library for multimedia functions
 
 int main() {
-    
-    cout << "Music is now playing...\n";
+    string i;
+    int count = 0; // Counter for each alternate 'Enter click'
+    do {
+        getline(cin, i); // Getting input by the user
+        if (count % 2 == 0) { //Playing the song the first time that Enter is pressed
+           cout << "Music Now Playing...\n";
+           PlaySound( // I used AI to comment lines 14-17
+               TEXT("Mzg1ODMxNTIzMzg1ODM3_JzthsfvUY24.wav"), // The filename of the sound to be played
+               NULL,                                         // Indicates the sound resource is specified by filename
+               SND_FILENAME | SND_ASYNC | SND_LOOP           // Flags: SND_FILENAME (specifies filename), SND_ASYNC (play asynchronously), SND_LOOP (loop the sound)
+           );
+           count += 1; // Incramenting counter so that music pauses next time Enter is pressed
+        }
 
-    // Play the specified sound file asynchronously and loop it continuously
-    PlaySound( // I used AI to comment lines 14-17
-        TEXT("Mzg1ODMxNTIzMzg1ODM3_JzthsfvUY24.wav"), // The filename of the sound to be played
-        NULL,                                         // Indicates the sound resource is specified by filename
-        SND_FILENAME | SND_ASYNC | SND_LOOP           // Flags: SND_FILENAME (specifies filename), SND_ASYNC (play asynchronously), SND_LOOP (loop the sound)
-    );
-
-    string inp;  // Declare a string variable to store user input
-
-    // Waits for the user to press Enter
-    getline(cin, inp);
-    PlaySound(0, 0, 0); // Stops the music
-    cout << "Music Paused...\n"; 
-
-  
-    getline(cin, inp); // Waits for the user to click Enter again
-    cout << "Music Unpaused...\n";
-    PlaySound(TEXT("Mzg1ODMxNTIzMzg1ODM3_JzthsfvUY24.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP); //Song resumes
+        else {
+            PlaySound(0, 0, 0); // Stops the music
+            cout << "Music Paused...\n";
+            count += 1; // Incramenting counter so that music plays next time Enter is pressed
+        }
 
 
-    // Final input
-    getline(cin, inp);
+    } while (true);
 
 }
